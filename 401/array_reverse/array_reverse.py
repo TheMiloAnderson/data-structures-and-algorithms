22c23507b1bde8accc90f4b36941a2a5ffbb8891
TheMiloAnderson <themiloanderson@gmail.com>
2019-02-18 17:09:55 -0800

    updated README

--- 401/array_reverse/array_reverse.py
+++ 401/array_reverse/array_reverse.py
@@ -1,8 +1,8 @@
-def reverseArray(arr):
+def reverse_array(arr):
     output = []
     for i in range(len(arr), 0, -1):
         output.append(arr[i - 1])
     return output
 
 arr = [3, 6, 9, 12, 15, 18]
-print(reverseArray(arr))
+print(reverse_array(arr))
