f96fe60c48fb18dfcba87494f11ad90cf377dd8f
TheMiloAnderson <themiloanderson@gmail.com>
2019-02-19 13:48:56 -0800

    added third test, fixed function so it passes tests

--- 401/array_shift/array_shift.py
+++ 401/array_shift/array_shift.py
@@ -4,11 +4,12 @@ A function that adds a value to the middle index of an array
 
 def insert_shift_array(arr, val):
     """A function that adds a value to the middle index of an array"""
-    mid = len(arr) // 2
+    mid = (len(arr) + 1) // 2
     new_arr = []
     for i in range(mid):
-        new_arr[i] = arr[i]
-    new_arr[mid] = val
-    for i in range(mid, len(arr) - 1):
-        new_arr[i + 1] = arr[i]
-    return rew_arr
+        print(i, arr[i])
+        new_arr.append(arr[i])
+    new_arr.append(val)
+    for i in range(mid, len(arr)):
+        new_arr.append(arr[i])
+    return new_arr
--- 401/array_shift/array_shift_test.py
+++ 401/array_shift/array_shift_test.py
@@ -1,13 +1,19 @@
 from array_shift import insert_shift_array
 
 def test_even_len_array_given():
-    """test array where length is even number"""
+    """ test array where length is even number """
     actual = insert_shift_array([1, 2, 3, 4, 5, 6], 99)
     expected = [1, 2, 3, 99, 4, 5, 6]
     assert actual == expected
 
 def test_odd_len_array_given():
-    """test array where length is odd number"""
+    """ test array where length is odd number """
     actual = insert_shift_array([1, 2, 3, 4, 5], 99)
     expected = [1, 2, 3, 99, 4, 5]
+    assert actual == expected
+
+def test_0_len_array_given():
+    """ test array where length is 0 """
+    actual = insert_shift_array([], 99)
+    expected = [99]
     assert actual == expected
