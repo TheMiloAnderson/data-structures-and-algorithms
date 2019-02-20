96dc4c656277a13b5a91a61cb6cc248190677f0c
TheMiloAnderson <themiloanderson@gmail.com>
2019-02-20 14:17:50 -0800

    wrote binary search tests

---
+++ 401/array_binary_search/array_binary_search_test.py
@@ -0,0 +1,22 @@
+from array_binary_search import array_binary_search
+
+
+def test_binary_search_small():
+    arr = [1, 2, 3, 4]
+    actual = array_binary_search(arr, 3)
+    expected = 2
+    assert actual == expected
+
+
+def test_binary_search_large():
+    arr = [i * 3 for i in range(100)]
+    actual = array_binary_search(arr, 21)
+    expected = 6
+    assert actual == expected
+
+
+def test_binary_search_not_found():
+    arr = [4, 8, 12, 16, 20, 24, 28]
+    actual = array_binary_search(arr, 15)
+    expected = -1
+    assert actual == expected
