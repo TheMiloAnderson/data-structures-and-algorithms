14a360a04981db610327362840f08b5813f3b28e
TheMiloAnderson <themiloanderson@gmail.com>
2019-02-20 14:23:36 -0800

    wrote binary search function

--- 401/array_binary_search/array_binary_search_test.py
+++ 401/array_binary_search/array_binary_search_test.py
@@ -2,6 +2,7 @@ from array_binary_search import array_binary_search
 
 
 def test_binary_search_small():
+    """ test with small array """
     arr = [1, 2, 3, 4]
     actual = array_binary_search(arr, 3)
     expected = 2
@@ -9,6 +10,7 @@ def test_binary_search_small():
 
 
 def test_binary_search_large():
+    """ test with larger array """
     arr = [i * 3 for i in range(100)]
     actual = array_binary_search(arr, 21)
     expected = 6
@@ -16,6 +18,7 @@ def test_binary_search_large():
 
 
 def test_binary_search_not_found():
+    """ test when value will not be found """
     arr = [4, 8, 12, 16, 20, 24, 28]
     actual = array_binary_search(arr, 15)
     expected = -1
