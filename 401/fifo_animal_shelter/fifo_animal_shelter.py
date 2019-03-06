3a7ea9228222ca1db20edefdf7d17210511dc5d0
TheMiloAnderson <themiloanderson@gmail.com>
2019-03-05 23:34:24 -0800

    enqueue, dequeue, and tests are working

--- 401/fifo_animal_shelter/fifo_animal_shelter.py
+++ 401/fifo_animal_shelter/fifo_animal_shelter.py
@@ -8,27 +8,27 @@ class AnimalShelter(object):
         self._queue = Queue()
 
     def enqueue(self, animal):
-        node = Node(animal)
-        if not self._queue.rear:
-            self._queue.rear = node
-            self._queue.front = node
-        else:
-            self._queue.rear.next = node
-            self._queue.rear = node
+        self._queue.enqueue(animal)
 
     def dequeue(self, pref):
+        if pref not in ('dog', 'cat'):
+            return None
         if self._queue.front:
-            prev = None
             curr = self._queue.front
-            while curr.value.type != pref:
+            prev = None
+            while curr.value.species != pref:
                 prev = curr
                 curr = curr.next
-            prev.next = curr.next
+            if prev:
+                prev.next = curr.next
+            else:
+                self._queue.front = curr.next
             curr.next = None
             return curr.value
         return None
 
 
 class Animal(object):
-    def __init__(self, type):
-        self.type = type
+    def __init__(self, species, name):
+        self.name = name
+        self.species = species
--- 401/fifo_animal_shelter/tests/fifo_animal_shelter_test.py
+++ 401/fifo_animal_shelter/tests/fifo_animal_shelter_test.py
@@ -1,13 +1,43 @@
 from fifo_animal_shelter.fifo_animal_shelter import Animal, AnimalShelter
-# from stacks_and_queues.stacks_and_queues import Queue, Node
-
-import sys
-sys.path.insert(
-    0, '/home/milo/codefellows/data-structures-and-algorithms/401/')
 
 
 def test_enqueue_one():
-    cat = Animal('cat')
     fas = AnimalShelter()
-    fas.enqueue(cat)
-    assert fas.front.value.type == 'cat'
+    fas.enqueue(Animal('cat', 'a'))
+    assert fas.dequeue('cat').name == 'a'
+
+
+def test_enqueue_many():
+    fas = AnimalShelter()
+    fas.enqueue(Animal('cat', 'a'))
+    fas.enqueue(Animal('cat', 'b'))
+    fas.enqueue(Animal('cat', 'c'))
+    assert fas.dequeue('cat').name == 'a'
+    assert fas.dequeue('cat').name == 'b'
+    assert fas.dequeue('cat').name == 'c'
+    assert not fas.dequeue('cat')
+
+
+def test_dequeue_both():
+    fas = AnimalShelter()
+    fas.enqueue(Animal('cat', 'a'))
+    fas.enqueue(Animal('cat', 'b'))
+    fas.enqueue(Animal('dog', 'c'))
+    fas.enqueue(Animal('cat', 'd'))
+    fas.enqueue(Animal('dog', 'e'))
+    fas.enqueue(Animal('dog', 'f'))
+    assert fas.dequeue('cat').name == 'a'
+    assert fas.dequeue('dog').name == 'c'
+    assert fas.dequeue('cat').name == 'b'
+    assert fas.dequeue('dog').name == 'e'
+    assert fas.dequeue('cat').name == 'd'
+    assert fas.dequeue('dog').name == 'f'
+    assert not fas.dequeue('cat')
+
+
+def test_dequeue_neither():
+    fas = AnimalShelter()
+    fas.enqueue(Animal('cat', 'a'))
+    fas.enqueue(Animal('parrot', 'b'))
+    fas.enqueue(Animal('dog', 'c'))
+    assert not fas.dequeue('parrot')
