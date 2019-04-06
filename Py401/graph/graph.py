db3428deae4a13ab82c75982244a9ff3daf7f2b8
TheMiloAnderson <themiloanderson@gmail.com>
2019-04-05 17:51:07 -0700

    wrote Graph & Vertex classes; methods pass all tests

---
+++ Py401/graph/graph.py
@@ -0,0 +1,47 @@
+from linked_list.linked_list import LinkedList
+
+
+class Graph(object):
+    """ Implements a graph """
+
+    def __init__(self):
+        self._vertices = set()
+
+    def add_vertex(self, val):
+        vert = Vertex(val)
+        self._vertices.add(vert)
+        return vert
+
+    def add_edge(self, vert1, vert2, weight=0):
+        if vert1 in self._vertices and vert2 in self._vertices:
+            vert1.adjacencies.append((vert2, weight))
+            vert2.adjacencies.append((vert1, weight))
+
+    def get_vertices(self):
+        return self._vertices
+
+    def get_neighbors(self, vert):
+        return vert.adjacencies
+
+    def size(self):
+        return len(self._vertices)
+
+    def __repr__(self):
+        return str(self.__class__) + str(self.__dict__)
+
+    def __str__(self):
+        return f'<Graph object with {len(self._vertices)} vertices>'
+
+
+class Vertex(object):
+    """ Implements a vertex object, for use in graphs """
+
+    def __init__(self, val):
+        self.value = val
+        self.adjacencies = []
+
+    def __repr__(self):
+        return str(self.__class__) + str(self.__dict__)
+
+    def __str__(self):
+        return f'<Vertex object {self.value}>'
---
+++ Py401/graph/tests/graph_test.py
@@ -0,0 +1,93 @@
+from graph.graph import Graph
+
+
+def test_class():
+    assert Graph
+
+
+def test_instance():
+    assert Graph()
+
+
+def test_add_one_vertext():
+    g = Graph()
+    v = g.add_vertex('A')
+    assert v in g.get_vertices()
+
+
+def test_add_two_vertices():
+    g = Graph()
+    v1 = g.add_vertex('A')
+    v2 = g.add_vertex('B')
+    assert v1.value == 'A'
+    assert v1 in g.get_vertices()
+    assert v2.value == 'B'
+    assert v2 in g.get_vertices()
+
+
+def test_add_edge():
+    g = Graph()
+    apple = g.add_vertex('apple')
+    banana = g.add_vertex('banana')
+    g.add_edge(apple, banana, 4)
+    for v in g.get_vertices():
+        if v.value == 'apple':
+            assert (banana, 4) in v.adjacencies
+        if v.value == 'banana':
+            assert (apple, 4) in v.adjacencies
+
+
+def test_add_weightless_edge():
+    g = Graph()
+    apple = g.add_vertex('apple')
+    banana = g.add_vertex('banana')
+    g.add_edge(apple, banana)
+    for v in g.get_vertices():
+        if v.value == 'apple':
+            assert (banana, 0) in v.adjacencies
+        if v.value == 'banana':
+            assert (apple, 0) in v.adjacencies
+
+
+def test_add_edge_missing_vertex():
+    g = Graph()
+    h = Graph()
+    apple = g.add_vertex('apple')
+    banana = h.add_vertex('banana')
+    g.add_edge(apple, banana, 4)
+    for v in g.get_vertices():
+        if v.value == 'apple':
+            assert v.adjacencies == []
+
+
+def test_get_vertices():
+    g = Graph()
+    apple = g.add_vertex('apple')
+    banana = g.add_vertex('banana')
+    vertices = g.get_vertices()
+    assert vertices == {apple, banana}
+
+
+def test_get_neighbors():
+    g = Graph()
+    apple = g.add_vertex('apple')
+    banana = g.add_vertex('banana')
+    cucumber = g.add_vertex('cucumber')
+    g.add_edge(apple, banana, 4)
+    g.add_edge(apple, cucumber, 5)
+    neighbors = g.get_neighbors(apple)
+    assert neighbors == [(banana, 4), (cucumber, 5)]
+
+
+def test_size():
+    g = Graph()
+    g.add_vertex('apple')
+    g.add_vertex('banana')
+    g.add_vertex('cucumber')
+    assert g.size() == 3
+
+
+def test_empty():
+    g = Graph()
+    assert g.get_vertices() == set()
+    assert g.size() == 0
