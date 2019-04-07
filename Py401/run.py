b506f73c37cb6ec72e47a94ab353b49570ec8792
TheMiloAnderson <themiloanderson@gmail.com>
2019-04-06 17:00:34 -0700

    reorder towers to improve efficiency of odd-size stacks

--- Py401/run.py
+++ Py401/run.py
@@ -30,9 +30,14 @@ def setup_towers(n):
     a = Tower('A')
     b = Tower('B')
     c = Tower('C')
-    a.nxt = b
-    b.nxt = c
-    c.nxt = a
+    if n % 2 == 0:
+        a.nxt = b
+        b.nxt = c
+        c.nxt = a
+    else:
+        a.nxt = c
+        b.nxt = a
+        c.nxt = b
     discs = [Disc(i) for i in range(n, 0, -1)]
     for i in discs:
         a.push(i)
@@ -45,24 +50,22 @@ def setup_towers(n):
     print(f'Congratulations, you won in {count} moves!')
 
 
-for tower in setup_towers(16):
-    # input('----- move -----')
+for tower in setup_towers(int(input('How many discs in the stack? '))):
+    input('----- move -----')
     print(tower, tower.nxt, tower.nxt.nxt, sep='\n')
     for nxt in [tower.nxt, tower.nxt.nxt]:
         disc = tower.peek()
-        # skip if no disc to move
-        if disc:
+        # skip if no disc to move; don't reverse previous move
+        if disc and disc.backwards != nxt:
             nxt_disc = nxt.peek()
-            # don't go backwards
-            if disc.backwards != nxt:
-                # move into empty stacks
-                if not nxt_disc:
-                    print(f'move {disc.num} from {tower.id} to {nxt.id}')
-                    nxt.push(tower.pop())
-                    disc.backwards = tower
-                # only move odd to even, even to odd, and smaller to larger
-                elif disc.num % 2 != nxt_disc.num % 2 and disc.num < nxt_disc.num:
-                    print(f'move {disc.num} from {tower.id} to {nxt.id}')
-                    nxt.push(tower.pop())
-                    disc.backwards = tower
+            # move into empty stacks
+            if not nxt_disc:
+                print(f'move {disc.num} from {tower.id} to {nxt.id}')
+                nxt.push(tower.pop())
+                disc.backwards = tower
+            # only move odd to even, even to odd, and smaller to larger
+            elif disc.num % 2 != nxt_disc.num % 2 and disc.num < nxt_disc.num:
+                print(f'move {disc.num} from {tower.id} to {nxt.id}')
+                nxt.push(tower.pop())
+                disc.backwards = tower
     print(tower, tower.nxt, tower.nxt.nxt, sep='\n')
